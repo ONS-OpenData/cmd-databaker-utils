a96233b3164cfde48fad877a0a8b3abe22f3c452
mikeAdams <mikelovesbooks@gmail.com>
2018-02-24 14:24:47 +0000

    Trying find packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
   name = 'databakerUtils',
-  packages = ['databakerUtils'],
+  packages = packages=find_packages()
   version = '0.1',
-  description = 'Some additional utilities for using databaker with ONS digital publishing',
+  description = 'Some additional utilities for using databaker within ONS digital publishing',
   author = 'Michael Adams',
   author_email = 'michael.adams@ons.gov.uk',
   url = 'https://github.com/ONS-OpenData/databakerUtils',
   download_url = 'https://github.com/ONS-OpenData/databakerUtils/archive/0.1.tar.gz',
   keywords = ['databaker', 'addon', 'utility'],
   classifiers = [],
-  include_package_data=True
 )
