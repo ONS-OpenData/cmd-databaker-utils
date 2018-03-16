9bd23bf84e5c4c84eb9c8cf87b01833bf434f40d
mikeAdams <mikelovesbooks@gmail.com>
2018-03-16 19:37:09 +0000

    Correction. DATAMARKING is DATAMARKER

--- databakerUtils/writers.py
+++ databakerUtils/writers.py
@@ -7,7 +7,7 @@ into a CSV strutured to the ONS digital publishing v4 specification.
 We're going to drop old columns as we add new ones, to minimise memory footprint.
 """
 
-def v4Writer(OutputName, df):
+def v4Writer(OutputName, df, asFrame=False):
 
     print('Extracting data structured as v4.')
 
@@ -18,11 +18,11 @@ def v4Writer(OutputName, df):
 
     # TODO - unlikely to be efficient. We should already know.
     hasDataMarker = False
-    if len(df['DATAMARKING'].unique()) > 1:
+    if len(df['DATAMARKER'].unique()) > 1:
         addColCount += 1
         hasDataMarker = True
     else:
-        df = df.drop('DATAMARKING', axis=1)
+        df = df.drop('DATAMARKER', axis=1)
 
     # Lets build our new dataframe
     newDf = pd.DataFrame()
@@ -30,8 +30,8 @@ def v4Writer(OutputName, df):
     df = df.drop('OBS', axis=1)
 
     if hasDataMarker:
-        newDf['Data_Marking'] = df['DATAMARKING']
-        df = df.drop('DATAMARKING', axis=1)
+        newDf['Data_Marking'] = df['DATAMARKER']
+        df = df.drop('DATAMARKER', axis=1)
 
     # Add quality measures
     # iterate and look for column headers to output prior to time
@@ -57,6 +57,10 @@ def v4Writer(OutputName, df):
         newDf[topic + '_codelist'] = ''
         newDf[topic] = df[topic]
 
+    # Optional return as dataframe, need keyword argument: asFrame=True
+    if asFrame:
+        return newDf
+
     newDf.to_csv(OutputName, encoding='utf-8', index=False)
-    print('V4 file created.')
+    print('V4 file written as:' + OutputName)
 
