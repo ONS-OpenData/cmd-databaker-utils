ad588a694f0cbfeecfcbf7a31dbc8eb1aff488b6
James Bryant <jamesbryant792@gmail.com>
2019-02-28 15:43:45 +0000

    tidied up functions

--- databakerUtils/sparsityFunctions.py
+++ databakerUtils/sparsityFunctions.py
@@ -12,6 +12,10 @@ def SparsityFiller(csv, DataMarker = '.'):
     df = pd.read_csv(csv, dtype = str)
     outputFile = csv[:-4] + '-without-sparsity.csv'
     
+    #Make sure is a v4 file - first column is V4
+    if not df.columns[0].startswith('V'):
+        raise Exception('Doesn\'t look like a V4 file')
+    
     #first a quick check to see if dataset is actually sparse
     v4marker = int(df.columns[0][-1])   #only interested in codelist columns
     columnList = list(df.columns)
@@ -24,9 +28,6 @@ def SparsityFiller(csv, DataMarker = '.'):
     numberOfObs = df.index.size
     if unsparseLength == numberOfObs:
         raise Exception('Dataset looks complete..')
-        
-    #quick check to make sure code will accept this many dimensions
-    NumberOfDimensionsCheck(columnCodeList)
     
     #list of lists of unique values for each dimension
     uniqueListOfCodesInColumns = UniqueListOfCodesInColumns(df, columnCodeList)
@@ -63,37 +64,9 @@ def UniqueListOfCodesInColumns(df, columnCodeList):
     returns a list of lists of unique values for each dimension
     '''
     #all unique values from each code column
-    if len(columnCodeList) == 3:
-        firstList = list(df[columnCodeList[0]].unique())
-        secondList = list(df[columnCodeList[1]].unique())
-        thirdList = list(df[columnCodeList[2]].unique())
-        allList = [firstList, secondList, thirdList]
-        
-    elif len(columnCodeList) == 4:
-        firstList = list(df[columnCodeList[0]].unique())
-        secondList = list(df[columnCodeList[1]].unique())
-        thirdList = list(df[columnCodeList[2]].unique())
-        forthList = list(df[columnCodeList[3]].unique())
-        allList = [firstList, secondList, thirdList, forthList]
-        
-        
-    elif len(columnCodeList) == 5:
-        firstList = list(df[columnCodeList[0]].unique())
-        secondList = list(df[columnCodeList[1]].unique())
-        thirdList = list(df[columnCodeList[2]].unique())
-        forthList = list(df[columnCodeList[3]].unique())
-        fithList = list(df[columnCodeList[4]].unique())
-        allList = [firstList, secondList, thirdList, forthList, fithList]
-        
-    elif len(columnCodeList) == 6:
-        firstList = list(df[columnCodeList[0]].unique())
-        secondList = list(df[columnCodeList[1]].unique())
-        thirdList = list(df[columnCodeList[2]].unique())
-        forthList = list(df[columnCodeList[3]].unique())
-        fithList = list(df[columnCodeList[4]].unique())
-        sixthList = list(df[columnCodeList[5]].unique())
-        allList = [firstList, secondList, thirdList, forthList, fithList, sixthList]
-    
+    allList = []
+    for column in columnCodeList:
+        allList.append(list(df[column].unique()))
     return allList
     
     
@@ -101,59 +74,24 @@ def ListsToFillDataDict(uniqueListOfCodesInColumns):
     '''
     returns a list of lists to be used to fill a dict
     each list will be as long as the unsparse version
+    arranges it so each line will be a unique combination
+    currently quite hacky..
     '''
-    if len(uniqueListOfCodesInColumns) == 3:
-        firstList, secondList, thirdList = [], [], []
-        for first in uniqueListOfCodesInColumns[0]:
-            for second in uniqueListOfCodesInColumns[1]:
-                for third in uniqueListOfCodesInColumns[2]:
-                    firstList.append(first)
-                    secondList.append(second)
-                    thirdList.append(third)
-        allList = [firstList, secondList, thirdList]
-        
-    elif len(uniqueListOfCodesInColumns) == 4:
-        firstList, secondList, thirdList, forthList = [], [], [], []
-        for first in uniqueListOfCodesInColumns[0]:
-            for second in uniqueListOfCodesInColumns[1]:
-                for third in uniqueListOfCodesInColumns[2]:
-                    for forth in uniqueListOfCodesInColumns[3]:
-                        firstList.append(first)
-                        secondList.append(second)
-                        thirdList.append(third)
-                        forthList.append(forth)
-        allList = [firstList, secondList, thirdList, forthList]
-        
-    elif len(uniqueListOfCodesInColumns) == 5:
-        firstList, secondList, thirdList, forthList, fithList = [], [], [], [], []
-        for first in uniqueListOfCodesInColumns[0]:
-            for second in uniqueListOfCodesInColumns[1]:
-                for third in uniqueListOfCodesInColumns[2]:
-                    for forth in uniqueListOfCodesInColumns[3]:
-                        for fith in uniqueListOfCodesInColumns[4]:
-                            firstList.append(first)
-                            secondList.append(second)
-                            thirdList.append(third)
-                            forthList.append(forth)
-                            fithList.append(fith)
-        allList = [firstList, secondList, thirdList, forthList, fithList]
-    
-    elif len(uniqueListOfCodesInColumns) == 6:
-        firstList, secondList, thirdList, forthList, fithList, sixthList = [], [], [], [], [], []
-        for first in uniqueListOfCodesInColumns[0]:
-            for second in uniqueListOfCodesInColumns[1]:
-                for third in uniqueListOfCodesInColumns[2]:
-                    for forth in uniqueListOfCodesInColumns[3]:
-                        for fith in uniqueListOfCodesInColumns[4]:
-                            for sixth in uniqueListOfCodesInColumns[5]:
-                                firstList.append(first)
-                                secondList.append(second)
-                                thirdList.append(third)
-                                forthList.append(forth)
-                                fithList.append(fith)
-                                sixthList.append(sixth)
-        allList = [firstList, secondList, thirdList, forthList, fithList, sixthList]
-    
+    totalSize = 1       #length of unsparse version
+    for List in uniqueListOfCodesInColumns:
+        totalSize *= len(List)
+    allList = []
+    appendThisManyTimes = totalSize        #used to create unique combinations
+    for List in uniqueListOfCodesInColumns:
+        listToAppend = []
+        appendThisManyTimes /= len(List)
+        appendThisManyTimes = int(appendThisManyTimes)
+        listDuplicatesTimes = int(totalSize / appendThisManyTimes / len(List))    #will duplicate the listToAppend
+        for item in List:
+            for i in range(appendThisManyTimes):
+                listToAppend.append(item)
+        listToAppend *= listDuplicatesTimes
+        allList.append(listToAppend)
     return allList
 
 
@@ -161,37 +99,10 @@ def DataDict(columnCodeList, listsToFillDataDict):
     '''
     Creates a dict to fill a dataframe
     '''
-    if len(columnCodeList) == 3:
-        data = {
-                columnCodeList[0]:listsToFillDataDict[0],
-                columnCodeList[1]:listsToFillDataDict[1],
-                columnCodeList[2]:listsToFillDataDict[2]
-                }
-    elif len(columnCodeList) == 4:
-        data = {
-                columnCodeList[0]:listsToFillDataDict[0],
-                columnCodeList[1]:listsToFillDataDict[1],
-                columnCodeList[2]:listsToFillDataDict[2],
-                columnCodeList[3]:listsToFillDataDict[3]
-                }
-    elif len(columnCodeList) == 5:
-        data = {
-                columnCodeList[0]:listsToFillDataDict[0],
-                columnCodeList[1]:listsToFillDataDict[1],
-                columnCodeList[2]:listsToFillDataDict[2],
-                columnCodeList[3]:listsToFillDataDict[3],
-                columnCodeList[4]:listsToFillDataDict[4]
-                }
-    elif len(columnCodeList) == 6:
-        data = {
-                columnCodeList[0]:listsToFillDataDict[0],
-                columnCodeList[1]:listsToFillDataDict[1],
-                columnCodeList[2]:listsToFillDataDict[2],
-                columnCodeList[3]:listsToFillDataDict[3],
-                columnCodeList[4]:listsToFillDataDict[4],
-                columnCodeList[5]:listsToFillDataDict[5]
-                }
-    
+    data = {}
+    length = len(columnCodeList)
+    for i in range(length):
+        data.update({columnCodeList[i]:listsToFillDataDict[i]})
     return data
 
 
@@ -200,36 +111,11 @@ def DictsToSortLabels(df, columnLabelList, columnCodeList):
     creates a list of dicts that will be used to fill in the labels 
     (using the existing data from the v4)
     '''
-    if len(columnCodeList) == 3:
-        firstDict = dict(zip(df[columnCodeList[0]], df[columnLabelList[0]]))
-        secondDict = dict(zip(df[columnCodeList[1]], df[columnLabelList[1]]))
-        thirdDict = dict(zip(df[columnCodeList[2]], df[columnLabelList[2]]))
-        allList = [firstDict, secondDict, thirdDict]
-    
-    elif len(columnCodeList) == 4:
-        firstDict = dict(zip(df[columnCodeList[0]], df[columnLabelList[0]]))
-        secondDict = dict(zip(df[columnCodeList[1]], df[columnLabelList[1]]))
-        thirdDict = dict(zip(df[columnCodeList[2]], df[columnLabelList[2]]))
-        forthDict = dict(zip(df[columnCodeList[3]], df[columnLabelList[3]]))
-        allList = [firstDict, secondDict, thirdDict, forthDict]
-        
-    elif len(columnCodeList) == 5:
-        firstDict = dict(zip(df[columnCodeList[0]], df[columnLabelList[0]]))
-        secondDict = dict(zip(df[columnCodeList[1]], df[columnLabelList[1]]))
-        thirdDict = dict(zip(df[columnCodeList[2]], df[columnLabelList[2]]))
-        forthDict = dict(zip(df[columnCodeList[3]], df[columnLabelList[3]]))
-        fithDict = dict(zip(df[columnCodeList[4]], df[columnLabelList[4]]))
-        allList = [firstDict, secondDict, thirdDict, forthDict, fithDict]
-        
-    elif len(columnCodeList) == 6:
-        firstDict = dict(zip(df[columnCodeList[0]], df[columnLabelList[0]]))
-        secondDict = dict(zip(df[columnCodeList[1]], df[columnLabelList[1]]))
-        thirdDict = dict(zip(df[columnCodeList[2]], df[columnLabelList[2]]))
-        forthDict = dict(zip(df[columnCodeList[3]], df[columnLabelList[3]]))
-        fithDict = dict(zip(df[columnCodeList[4]], df[columnLabelList[4]]))
-        sixthDict = dict(zip(df[columnCodeList[5]], df[columnLabelList[5]]))
-        allList = [firstDict, secondDict, thirdDict, forthDict, fithDict, sixthDict]
-        
+    allList = []
+    length = len(columnCodeList)
+    for i in range(length):
+        newDict = dict(zip(df[columnCodeList[i]], df[columnLabelList[i]]))
+        allList.append(newDict)
     return allList
 
 
@@ -238,36 +124,10 @@ def ApplyingTheDicts(df, dictsToSortLabels, columnCodeList, columnLabelList):
     applys the dicts to populate the label columns
     '''
     newDF = df.copy()
-    if len(dictsToSortLabels) == 3:
-        firstLabel, secondLabel, thirdLabel = columnCodeList
-        newDF[columnLabelList[0]] = newDF[firstLabel].apply(lambda x:dictsToSortLabels[0][x])
-        newDF[columnLabelList[1]] = newDF[secondLabel].apply(lambda x:dictsToSortLabels[1][x])
-        newDF[columnLabelList[2]] = newDF[thirdLabel].apply(lambda x:dictsToSortLabels[2][x])
     
-    elif len(dictsToSortLabels) == 4:
-        firstLabel, secondLabel, thirdLabel, forthLabel = columnCodeList
-        newDF[columnLabelList[0]] = newDF[firstLabel].apply(lambda x:dictsToSortLabels[0][x])
-        newDF[columnLabelList[1]] = newDF[secondLabel].apply(lambda x:dictsToSortLabels[1][x])
-        newDF[columnLabelList[2]] = newDF[thirdLabel].apply(lambda x:dictsToSortLabels[2][x])
-        newDF[columnLabelList[3]] = newDF[forthLabel].apply(lambda x:dictsToSortLabels[3][x])
- 
-    elif len(dictsToSortLabels) == 5:
-        firstLabel, secondLabel, thirdLabel, forthLabel, fithLabel = columnCodeList
-        newDF[columnLabelList[0]] = newDF[firstLabel].apply(lambda x:dictsToSortLabels[0][x])
-        newDF[columnLabelList[1]] = newDF[secondLabel].apply(lambda x:dictsToSortLabels[1][x])
-        newDF[columnLabelList[2]] = newDF[thirdLabel].apply(lambda x:dictsToSortLabels[2][x])
-        newDF[columnLabelList[3]] = newDF[forthLabel].apply(lambda x:dictsToSortLabels[3][x])
-        newDF[columnLabelList[4]] = newDF[fithLabel].apply(lambda x:dictsToSortLabels[4][x])
-    
-    elif len(dictsToSortLabels) == 6:
-        firstLabel, secondLabel, thirdLabel, forthLabel, fithLabel, sixthLabel = columnCodeList
-        newDF[columnLabelList[0]] = newDF[firstLabel].apply(lambda x:dictsToSortLabels[0][x])
-        newDF[columnLabelList[1]] = newDF[secondLabel].apply(lambda x:dictsToSortLabels[1][x])
-        newDF[columnLabelList[2]] = newDF[thirdLabel].apply(lambda x:dictsToSortLabels[2][x])
-        newDF[columnLabelList[3]] = newDF[forthLabel].apply(lambda x:dictsToSortLabels[3][x])
-        newDF[columnLabelList[4]] = newDF[fithLabel].apply(lambda x:dictsToSortLabels[4][x])
-        newDF[columnLabelList[5]] = newDF[sixthLabel].apply(lambda x:dictsToSortLabels[5][x])
-        
+    length = len(dictsToSortLabels)
+    for i in range(length):
+        newDF[columnLabelList[i]] = newDF[columnCodeList[i]].apply(lambda x:dictsToSortLabels[i][x])
     return newDF
 
 
@@ -309,11 +169,3 @@ def IndexOfDuplicates(concatDF, columnCodeList):
     newDF = newDF.drop_duplicates()
     indexToKeep = list(newDF.index)
     return indexToKeep
-    
-
-def NumberOfDimensionsCheck(columnCodeList):
-    '''
-    code has been written to incorperate v4 files with this many dimensions
-    '''
-    if len(columnCodeList) not in [3, 4, 5, 6]:
-        raise Exception('Program not yet complete for {} dimensions'.format(len(columnCodeList)))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'databakerUtils',
   packages = ['databakerUtils'],
-  version = '0.1.27',
+  version = '0.1.28',
   description = 'Some additional utilities for using databaker within ONS digital publishing',
   author = 'Michael Adams',
   author_email = 'michael.adams@ons.gov.uk',
