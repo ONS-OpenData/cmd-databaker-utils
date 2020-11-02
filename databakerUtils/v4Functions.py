6169348fcf362ed28911413ab2197cfd21525b20
James Bryant <jamesbryant792@gmail.com>
2020-11-02 15:23:04 +0000

    updated code list name

--- databakerUtils/v4Functions.py
+++ databakerUtils/v4Functions.py
@@ -7,7 +7,7 @@ import requests
 import pandas as pd
 import numpy as np
 
-wholeDict = requests.get('https://api.beta.ons.gov.uk/v1/code-lists/admin-geography/editions/one-off/codes').json()
+wholeDict = requests.get('https://api.beta.ons.gov.uk/v1/code-lists/administrative-geography/editions/one-off/codes').json()
 adminDict = {}
 for item in wholeDict['items']:
     adminDict.update({item['code']:item['label']})
