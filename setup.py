a5a80e2da460d85558d814c051c0e40f035ef0a2
James Bryant <jamesbryant792@gmail.com>
2021-02-01 16:02:58 +0000

    version 1.49

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'databakerUtils',
   packages = ['databakerUtils'],
-  version = '0.1.48',
+  version = '0.1.49',
   description = 'Some additional utilities for using databaker within ONS digital publishing',
   author = 'Michael Adams',
   author_email = 'michael.adams@ons.gov.uk',
